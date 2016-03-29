1e471e1d7cbdae67bbd8eec473386139544b5084
Randy Torres <torresr14@gmail.com>
2016-03-28 22:54:00 -0700

    finished

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-	{ title: "You Don't know Js" },
-	{ title: "Harry Potter" },
-	{ title: "The Dark Tower" },
-	{ title: "Eloquent Ruby" }
+	{ title: "You Don't know Js", pages: 101 },
+	{ title: "Harry Potter", pages: 2902 },
+	{ title: "The Dark Tower", pages: 203 },
+	{ title: "Eloquent Ruby", pages: 1 }
   ]
 }
